848073912d63bd4ffad26f144c420f4031e2b237
Gallo <ygallo@microstrategy.com>
2021-04-23 19:13:57 -0300

    ¨carrito¨

--- src/FPages/Carrito.js
+++ src/FPages/Carrito.js
@@ -9,12 +9,16 @@ import TableRow from '@material-ui/core/TableRow';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import order from '../Data/order.js'; 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
+  title: {
+    paddingBottom: theme.spacing(2),
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
@@ -57,10 +61,10 @@ function Carrito() {
       <main >
         {/* Grilla  */}
         <Container className={classes.cardGrid} maxWidth="md">
-        <Typography gutterBottom variant="h5" >
-          Recent Orders
+        <Typography gutterBottom variant="h5"  className={classes.title} >
+          Compras 
         </Typography>
-        <Table size="small">
+        <Table size="small" className={classes.heroContent}>
           <TableHead>
             <TableRow>
               <TableCell>Producto</TableCell>
@@ -70,27 +74,44 @@ function Carrito() {
               <TableCell> Cantidad </TableCell>
               <TableCell> Precio Unitario</TableCell>
               <TableCell align="right"> Subtotal </TableCell>
+              <TableCell >   </TableCell>
            </TableRow>
           </TableHead>
           <TableBody>
            {order.items.map((x) => (
               <TableRow key={x.producto_id}>
+                <TableCell>
+                <Avatar variant="square" src={x.product.img} />
+                
+                </TableCell>
                 <TableCell>{x.product.name}</TableCell>
-                <TableCell>{x.product.img}</TableCell>
                 <TableCell>{x.size}</TableCell>
                 <TableCell>{x.color}</TableCell>
                 <TableCell>{x.color}</TableCell>
-                <TableCell align="right">{x.subtotal}</TableCell>
-                <TableCell align="right" >{x.subtotal}</TableCell>
+                <TableCell align="right">  $ {ccyFormat (x.subtotal)}  </TableCell>
+                <TableCell align="right" > $ {ccyFormat (x.subtotal)} </TableCell>
+                <TableCell >
+                  <IconButton aria-label="delete" className={classes.margin}>
+                   <DeleteIcon />
+                  </IconButton>
+                </TableCell>
               </TableRow>
             ))}
             <TableRow >
-            <TableCell  colSpan={2}>Total</TableCell>
-            <TableCell align="right">{ccyFormat(order.total)}</TableCell>
+            <TableCell  colSpan={2}> <Typography variant="h6"  component="h6" > Total</Typography></TableCell>
+            <TableCell align="right"><Typography component="h6" > $ {ccyFormat(order.total)} </Typography></TableCell>
             </TableRow>     
           </TableBody>
         </Table>
+        <Container className={classes.cardGrid}   maxWidth="lg" >
+        <Button variant="contained" color="primary" >
+          comprar
+        </Button>
+        </Container>
       </Container>
+      
+
+
       </main>
       {/* Footer */}
       <div >
