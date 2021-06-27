ec101e9cc53062895f24fc0bfbf74cd40c18b632
fguerrero <fguerrero2@gmail.com>
2021-06-27 15:53:28 -0300

    Ajustes en estructura de array de items

--- backend/models/Orden.model.js
+++ backend/models/Orden.model.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose')
 var mongoosePaginate = require('mongoose-paginate')
 
+var OrdenItemSchema = new mongoose.Schema({
+    item_id: String, 
+    size: String,
+    color: String, 
+    quantity: Number, 
+    subtotal : Number, 
+    product_id:String, 
+    name: String, 
+    price:Number
+})
 
 var OrdenSchema = new mongoose.Schema({
     nro_orden: String,
@@ -12,7 +22,7 @@ var OrdenSchema = new mongoose.Schema({
     direccion_entrega: String,
     estado:String,
     fecha_entrega: String,
-    items: [{item_id: String, size: String,color: String, quantity: Number, subtotal : Number, product_id:String, name: String, price:Number}]
+    items: [OrdenItemSchema]
 })
 
 OrdenSchema.plugin(mongoosePaginate)
